00527387a64c3e1c57c1533ff6f8cd9440785411
gettogetto <643274003@qq.com>
2016-03-13 17:00:14 +0800

    fixed

--- SingleApplication.cpp
+++ SingleApplication.cpp
@@ -10,44 +10,24 @@ SingleApplication::SingleApplication(int argc, char *argv[])
     , _isRunning(false)
     , _localServer(NULL) {
 
-    
     _serverName = QFileInfo(QCoreApplication::applicationFilePath()).fileName();
 
     _initLocalConnection();
 }
-
-
-
-bool SingleApplication::isRunning() {
-    return _isRunning;
-}
-
-
-void SingleApplication::_newLocalConnection() {
-    QLocalSocket *socket = _localServer->nextPendingConnection();
-    if(socket) {
-        socket->waitForReadyRead(2*TIME_OUT);
-        delete socket;
-
-
-        _activateWindow();
-    }
-}
-
 
 void SingleApplication::_initLocalConnection() {
     _isRunning = false;    
 
     QLocalSocket socket;
     socket.connectToServer(_serverName);
-    if(socket.waitForConnected(TIME_OUT)) {
-
+	//Qt assistant:Waits until the socket is connected, up to msecs milliseconds. If the connection has been established, this function returns true; otherwise it returns false.
+    if(socket.waitForConnected(TIME_OUT)) {//连接建立
         _isRunning = true;
 
         return;
     }
 
-
+    //连接不上服务器，就创建一个
     _newLocalServer();
 }
 
@@ -56,7 +36,6 @@ void SingleApplication::_newLocalServer() {
     connect(_localServer, SIGNAL(newConnection()), this, SLOT(_newLocalConnection()));
 	
     if(!_localServer->listen(_serverName)) {
-
         if(_localServer->serverError() == QAbstractSocket::AddressInUseError) {
             QLocalServer::removeServer(_serverName); 
             _localServer->listen(_serverName); 
@@ -64,11 +43,24 @@ void SingleApplication::_newLocalServer() {
     }
 }
 
+void SingleApplication::_newLocalConnection() {
+	//Qt assistant:0 is returned if this function is called when there are no pending connections.
+    QLocalSocket *socket = _localServer->nextPendingConnection();
+    if(socket!=nullptr) {
+        socket->waitForReadyRead(2*TIME_OUT);
+        delete socket;
+        _activateWindow();
+    }
+}
+
+bool SingleApplication::isRunning() {
+    return _isRunning;
+}
 
 void SingleApplication::_activateWindow() {
     if(w) {
         w->show();
         w->raise();
-        w->activateWindow(); 
+        w->activateWindow();
     }
 }
